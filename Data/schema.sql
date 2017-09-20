c91b30fbccd7867f0f0bf9478d904b684742e14a
bonnieho <bonnieho@rice.edu>
2017-09-19 20:03:44 -0500

    added auto_increment id key to schema query

--- Data/schema.sql
+++ Data/schema.sql
@@ -4,11 +4,12 @@ CREATE DATABASE hot_restaurant;
 USE hot_restaurant;
 
 CREATE TABLE user_rsvp(
+  user_id INT(4) AUTO_INCREMENT NOT NULL UNIQUE,
   screen_id VARCHAR(8) NOT NULL UNIQUE,
   guest_name VARCHAR(36) NOT NULL,
   guest_email VARCHAR(36) NOT NULL,
   guest_phone VARCHAR(10) NOT NULL,
-  PRIMARY KEY (screen_id)
+  PRIMARY KEY (user_id)
 );
 
 
